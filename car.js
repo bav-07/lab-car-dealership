25ab17c18f6f253e2c309247305be926239cfb43
Bavaharsan Nagarajah <bavaharsan.nagarajah@gmail.com>
2023-01-11 17:45:36 +0000

    added car class and tests for accessing car object properties

---
+++ car.js
@@ -0,0 +1,8 @@
+const Car = function(manufacturer, model, price, engine){
+    this.manufacturer = manufacturer;
+    this.model = model;
+    this.price = price;
+    this.engine = engine;    
+};
+
+module.exports = {Car};
---
+++ car.test.js
@@ -0,0 +1,36 @@
+const {Car} = require('./car');
+
+let car;
+
+beforeEach(() => {
+    AE86 = new Car("Toyota", "Trueno", 110000, "4A-GE");
+    R32 = new Car("Nissan", "Skyline", 30000, "2.6L RB");
+    FD = new Car("Mazda", "RX-7", 45000, "13B-REW");
+    Countach = new Car("Lamborghini", "Countach", 300000, "LP400");
+    Porsche911 = new Car("Porsche", "911", 250000, "3.3L");
+    F40 = new Car("Ferrari", "F40", 2500000, "2.9L V8");
+    E26 = new Car("BMW", "M1", 800000, "3.5L M88/1 I6");
+})
+
+describe('testing properties', () => {
+    test('can obtain manufacturer', () => {
+        const expected = "Nissan";
+        const actual = R32.manufacturer;
+        expect(actual).toBe(expected);
+    })
+    test('can obtain model', () => {
+        const expected = "RX-7";
+        const actual = FD.model;
+        expect(actual).toBe(expected);
+    })
+    test('can obtain price', () => {
+        const expected = 300000;
+        const actual = Countach.price;
+        expect(actual).toBe(expected);
+    })
+    test('can obtain engine', () => {
+        const expected = "2.9L V8";
+        const actual = F40.engine;
+        expect(actual).toBe(expected);
+    })
+})
