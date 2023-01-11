5b231fb2affd7953e77c274dba59c21ef4772189
Bavaharsan Nagarajah <bavaharsan.nagarajah@gmail.com>
2023-01-11 18:39:29 +0000

    can add car to stock, method tested

--- dealership.js
+++ dealership.js
@@ -10,4 +10,8 @@ Dealership.prototype.countCars = function() {
     return this.stock.length;
 }
 
+Dealership.prototype.addCarToStock = function(car) {
+    this.stock.push(car);
+}
+
 module.exports = {Dealership};
--- dealership.test.js
+++ dealership.test.js
@@ -1,8 +1,8 @@
 const {Car} = require('./car');
 const {Dealership} = require('./dealership');
 
-let AE86, R32, FD, Countach, Porsche911, F40, E26;
-let dealership;
+let AE86, R32, FD, Countach, Porsche911, F40, E26, DB9;
+let bavCars;
 let stock;
 
 beforeEach(() => {
@@ -13,14 +13,22 @@ beforeEach(() => {
     Porsche911 = new Car("Porsche", "911", 250000, "3.3L");
     F40 = new Car("Ferrari", "F40", 2500000, "2.9L V8");
     E26 = new Car("BMW", "M1", 800000, "3.5L M88/1 I6");
+    DB9 = new Car("Aston Martin", "DB9", 200000, "5.9L V12");
     stock = [AE86, R32, FD, Countach, Porsche911, F40, E26];
-    dealership = new Dealership("Bav's Cars", 9, stock);
+    bavCars = new Dealership("Bav's Cars", 9, stock);
 })
 
 describe('testing dealership stock methods', () => {
     test('can count cars in stock', () => {
         const expected = 7;
-        const actual = dealership.stock.length;
+        const actual = bavCars.countCars();
         expect(actual).toBe(expected);
     })
+    test('can add car to stock', () => {
+        bavCars.addCarToStock(DB9)
+        const expected = 8;
+        const actual = bavCars.countCars();
+        expect(actual).toBe(expected);
+    })
+    
 })
